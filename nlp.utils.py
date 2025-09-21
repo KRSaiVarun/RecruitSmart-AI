bfa107c2f8a039fb23f422a067766c4f64569d97
K.R SAI VARUN <krsaivarun@gmail.com>
2025-09-21 23:23:38 +0530

    Update nlp.utils.py

--- nlp.utils.py
+++ nlp.utils.py
@@ -1,5 +1,12 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    # Download automatically if missing
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def parse_resume_text(text):
     doc = nlp(text)
